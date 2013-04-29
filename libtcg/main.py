f089c2f6ed1cd7e610aa3de4aee74450cc2ade83
noname <samqhrobya@liquidid.net>
2013-04-29 12:46:48 +0900

    done

--- libtcg/main.py
+++ libtcg/main.py
@@ -1,72 +1,81 @@
-
-import email
-import email.header
-import imaplib
-import sys
-import os
-import shutil
-
-import xoauth
-from optparse import OptionParser
-import tempfile
+from common import open_conf
+import argparse
+import oauth2
 import json
-import os.path
-import subprocess
-
-consumer = xoauth.OAuthEntity('anonymous', 'anonymous')
-
+import os
+import imaplib
 MAX_FETCH = 20
+import sys
 
+def arg_parse():
+    "Parse the command line arguments"
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--filename', help='conf filename'
+            , default='')
+    parser.add_argument('-a','--add', action='store_true',default=False, 
+                        help='add mail conf' )
+    parser.add_argument('-m','--merge', action='store_true',default=False, 
+                        help='merge  confs to conf' )
+    parser.add_argument('-c','--check', action='store_true',default=False, 
+                        help='check mail' )
+    parser.add_argument('-u','--username', help='gmail username'
+            , default='')
+    parser.add_argument('filenames', metavar='filename', type=str, nargs='+',                                
+                     help='merge filename ')
+    return parser.parse_args()
 
 def main():
-    parser = OptionParser()
-    parser.add_option("-c", "--check", action="store_true", dest="check",
-                      help="check mail", default=False)
-    (options, args) = parser.parse_args()
+    args = arg_parse()
+    if args.filename == '':
+        pass
+    else:
+        filename = args.filename
+    if args.add:
+        config1={}
+        config1['user']= args.username
+        tmpc = open_conf(filename)
+        print '  %s' % oauth2.GeneratePermissionUrl(tmpc['client_id'] )
+        authorization_code = raw_input('Enter verification code: ')
+        response = oauth2.AuthorizeTokens(tmpc['client_id'], tmpc['client_secret'],
+                                authorization_code)
+        config1['refresh_token']= response['refresh_token']
+        file1 = open('config', 'w')
+        k=json.dump(config1,file1,indent=4)
+        file1.close()
+        print '\nconfig written.\n'
+    elif args.merge:
+        config1=[]
+        for f1 in args.filenames:
+            tmp1=open_conf(f1)
+            config1.append(tmp1)
+        file1 = open('config_merge', 'w')
+        k=json.dump(config1,file1,indent=4)
+        file1.close()
+    elif args.check:
+        cf1 = open_conf(filename)
+        client = cf1['client_id'],cf1['client_secret']
+        filename=args.filenames[0]
+        tmp1=open_conf(filename)
+        check_items(tmp1,client)
 
-    if options.check:
-        filename=args[0]
-        if os.path.splitext(filename)[1] == '.gpg':
-            ret= subprocess.Popen(args='gpg --output - %s' % filename, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()[0]
-            tmp1=json.loads(ret)
-        else:
-            file1 = open(filename,'r')
-            tmp1=json.load(file1)
-            file1.close()
-        check_items(tmp1)
-
-def check_items(tmp1):
+def check_items(tmp1,client):
     for it1 in tmp1:
-        class Config():
-            pass
-        config = Config()
-        for key2,item in it1['access_token'].items():
-            it1['access_token'][key2]=item.encode('ascii')
-        config.user=it1['user'].encode("ascii")
-        config.access_token=it1['access_token']
-        num = check_name(config)
+        num = check_name(it1,client)
         if not num > 0 :
             print '.',
             sys.stdout.flush()
 
-def check_name(config):
-    imap_hostname = 'imap.gmail.com'
-    config.google_accounts_url_generator = \
-            xoauth.GoogleAccountsUrlGenerator(config.user)
-    access_token = xoauth.OAuthEntity(config.access_token['key'], 
-            config.access_token['secret'])
+def check_name(config,client):
+    client_id,client_secret = client
+    user = config['user'].encode('ascii')
+    refresh_token = config['refresh_token'].encode('ascii')
+    response = oauth2.RefreshToken(client_id, client_secret, refresh_token)
 
-    class ImBad():
-        def write(self, msg): 
-            pass
-    sys.stdout = ImBad()
-    xoauth_string = xoauth.GenerateXOauthString(
-          consumer, access_token, config.user, 'IMAP',
-          xoauth_requestor_id=None, nonce=None, timestamp=None)
-    sys.stdout = sys.__stdout__
-
-    imap_conn = imaplib.IMAP4_SSL(imap_hostname)
-    imap_conn.authenticate('XOAUTH', lambda x: xoauth_string)
+    access_token = response['access_token']
+    auth_string = oauth2.GenerateOAuth2String(user, access_token,
+                             base64_encode=False)
+    imap_conn = imaplib.IMAP4_SSL('imap.gmail.com')
+    imap_conn.authenticate('XOAUTH2', lambda x: auth_string)
     imap_conn.select('INBOX', readonly=True)
     _, data = imap_conn.search(None, 'UNSEEN')
     unreads = data[0].split()
